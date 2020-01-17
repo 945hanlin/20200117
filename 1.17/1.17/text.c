6e5ed13e6f6a8bf24a51e55630e005b7e57a784e
945hanlin <328507716@qq.com>
2020-01-17 17:52:17 +0800

    2020117

---
+++ 1.17/1.17/text.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	//int i, j, a[2][3];
+	/*for (i = 0; i < 2; i++) {
+		for (j = 0; j < 3; j++){
+			a[i][j] = i * 3 + j + 1;
+		}
+	}*/
+	/*for (i = 0; i < 3; i++){
+		for (j = 0; j < 2; j++){
+			a[j][i] = j * 3 + i + 1;
+		}
+	}*/
+	/*for (i = 0; i < 6; i++){
+		a[i / 3][i % 3] = i + 1;
+	}*/
+	//for (i = 1; i <= 6; i++){
+	//	a[i][i] = i;
+	//}
+	//printf("%d", a[1][2]);
+	system("pause");
+	return 0;
+}
